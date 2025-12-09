c62979a5dda4db6aa75f407aefce9b7fcde622ca
Jared Whalen <jared.m.whalen@gmail.com>
2025-12-09 13:59:48 -0500

    Add video map with credits

--- src/lib/components/VideoPlayer.svelte
+++ src/lib/components/VideoPlayer.svelte
@@ -1,16 +1,40 @@
 <script>
     import { fade } from "svelte/transition";
-    let { view } = $props();
+    let { activeId } = $props();
+
+    $inspect(activeId);
+
+    let videos = {
+        "port_of_chancay_video": {
+            src: "port_of_chancay_video.mp4",
+            credit: "TKTK",
+        },
+        "port_of_santos_video": {
+            src: "port_of_santos_video.mp4",
+            credit: "PTKTK",
+        },
+    }
 
 </script>
 
 <div class="video-player" transition:fade>
-    <video src="videos/VIDEO-2025-08-28-12-10-10-700x700-bg.mp4" autoplay loop muted playsinline></video>
+    <video src={"videos/" + activeId + ".mp4"} autoplay loop muted playsinline></video>
+    <span class="credit">{videos[activeId].credit}</span>
 </div>
 
 <style>
     .video-player {
         width: 100%;
         height: 100%;
+    }
+
+    .credit {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        font-size: 12px;
+        color: #fff;
+        background: rgba(0, 0, 0, 0.5);
+        padding: 0.25rem;
     }
 </style>
