bd8bf9a7bb249a1facd4f707cbc823bd43d9ee8a
Jared Whalen <jared.m.whalen@gmail.com>
2025-12-14 19:12:49 -0500

    Updated credits

--- src/lib/data/videos.js
+++ src/lib/data/videos.js
@@ -1,19 +1,19 @@
 export const videos = {
     port_of_chancay_video: {
         src: "port_of_chancay_video.mp4",
-        credit: "Video by Christian E. Oliva Ladines",
+        credit: "Video by Christian E. Oliva Ladines, National Port Authority of Perú",
         muted: true,
         fullbleed: true
     },
     port_of_santos_video: {
         src: "port_of_santos_video.mp4",
-        credit: "Video by AC Drone",
+        credit: "Video by <a href='https://www.youtube.com/@ACDrone' target='_blank'>AC Drone</a>",
         muted: true,
         fullbleed: true
     },
     farmer_video: {
         src: "farmer_video_cc.mp4",
-        credit: "Video by David Vargas",
+        credit: "Video by John Bartman",
         muted: false,
     },
 };
